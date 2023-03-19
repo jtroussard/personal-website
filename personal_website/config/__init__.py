906a675a7121787790642b62f5ffe91af216e06f
Jacques <jacques.troussard@gmail.com>
2023-03-19 16:28:11 -0400

    DEV: Converted to Flask package application. Small UI/style tweaks

---
+++ personal_website/config/__init__.py
@@ -0,0 +1,10 @@
+class Config(object):
+    SERVER_NAME = "localhost:12031"
+    DEBUG = True
+    BASE_URL = "http://localhost:12031"
+
+
+class TestingConfig(object):
+    SERVER_NAME = "localhost:12031"
+    DEBUG = True
+    BASE_URL = "http://localhost:12031"
---
+++ personal_website/routes.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from flask import render_template, url_for
+from personal_website import app
+
+@app.route("/")
+@app.route("/home")
+def home():
+    """
+    Route: Home page.
+    :return: The home.html file is being returned.
+    """
+    return render_template("home.html", active_page="home")
+
+
+@app.route("/weight-tracker", methods=["GET"])
+def weight_tracker():
+    """
+    Route: Weight tracker flask application demo
+    """
+    return render_template("coming-soon.html")
