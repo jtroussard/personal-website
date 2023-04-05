9270e577faff0e34b5ba95be0e8394e90ff1bde9
gcpAgent <gcpAgent@personal-website.com>
2023-04-05 19:05:13 +0000

    wip

--- vars.sh
+++ vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export APP_DIR=/home/$(whoami)/service/personal-website/
-export VENV_DIR=/home/$(whoami)/service/personal-website/venv/
+export APP_DIR=/home/$(whoami)/service/personal-website
+export VENV_DIR=/home/$(whoami)/service/personal-website/venv
 export WORKER_COUNT=$((($(nproc)/2)+1))
