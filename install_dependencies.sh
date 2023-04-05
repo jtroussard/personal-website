f70223991faa862d8bfe49b1337d104bf50f697e
instance2 <instance2@personal-website.com>
2023-04-05 23:05:42 +0000

    on server changes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Checking if Nginx is installed..."
-if ! command -v nginx &> /dev/null
+if ! command -v sudo nginx &> /dev/null
 then
     echo "Nginx is not installed, updating repo list and installing..."
     sudo apt update && sudo apt install -y nginx
@@ -27,10 +27,28 @@ else
     echo "Python 3 is already installed."
 fi
 
+
+echo "Checking if pip3 is installed..."
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 is not installed, updating repo list and installing..."
+    sudo apt update && sudo apt install -y python3-pip
+else
+    echo "pip3 is already installed."
+fi
+
+echo "Checking if Python virtual environment is installed..."
+if ! dpkg -s python3-venv &> /dev/null
+then
+    echo "Python virtual environment is not installed, updating repo list and installing..."
+    sudo apt update && sudo apt install -y python3-venv
+else
+    echo "Python virtual environment is already installed."
+fi
+
+
 echo "Installation complete."
-echo "Nginx version:"
-nginx -v
-echo "Supervisor version:"
-supervisorctl -v
-echo "Python 3 version:"
-python3 --version
+echo "Nginx version: $(sudo nginx -v 2>&1 | grep -oP '(?<=nginx\/)[0-9]+\.[0-9]+\.[0-9]+')"
+echo "Supervisor version: $(supervisord -v)"
+echo "Python 3 version: $(python3 --version | cut -d ' ' -f 2)"
+
--- vars.sh
+++ vars.sh
@@ -2,4 +2,4 @@
 export APP_DIR=/home/$(whoami)/service/personal-website
 export VENV_DIR=/home/$(whoami)/service/personal-website/venv
 export WORKER_COUNT=$((($(nproc)/2)+1))
-export LOG_DIR="/var/log/personal-website/"
+export LOG_DIR=/var/log/personal-website/
