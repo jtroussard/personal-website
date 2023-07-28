12a6494ea3595901eeaf6a01d604fd2df9edd904
jacques troussard <jacques.troussard@gmail.com>
2023-07-28 11:33:42 -0400

    adds work and skills section interpolated values via data.json file, updates docs, css tweeks

--- personal_website/__init__.py
+++ personal_website/__init__.py
@@ -1,9 +1,18 @@
 # pylint: disable=wrong-import-position, missing-module-docstring
+import os
 from flask import Flask
-from personal_website.config import ProductionConfig
+# from personal_website.config import DevelopmentConfig
+
+# Helper functions
+def register_filters(app):
+    from personal_website.filters import get_icon_url
+    app.logger.info('registering filters')
+    app.jinja_env.filters['get_icon_url'] = get_icon_url
 
 app = Flask(__name__)
-app.config.from_object(ProductionConfig)
+# app.config.from_object(DevelopmentConfig)
+
+register_filters(app)
 
 # Avoid circular dependencies
 from personal_website import routes
--- personal_website/routes.py
+++ personal_website/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+import os, json
+from flask import render_template, url_for
 from personal_website import app
 
 @app.route("/")
@@ -8,8 +9,17 @@ def home():
     Route: Home page.
     :return: The home.html file is being returned.
     """
-    return render_template("home.html", active_page="home")
+    # Read the path to the JSON file from the environment variable
+    data_path = os.environ.get('PORTFOLIO_DATA_PATH')
+    print(data_path)
 
+    if data_path is None:
+        return "Error: Portfolio data path not set."
+
+    # Load the JSON data from the file
+    with open(data_path, 'r') as json_file:
+        portfolio_data = json.load(json_file)
+    return render_template("home.html", active_page="home", data=portfolio_data)
 
 @app.route("/weight-tracker", methods=["GET"])
 def weight_tracker():
