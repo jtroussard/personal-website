99abb2bf103bf926a7c4fd53d544ca8559ad0e65
jacques troussard <jacques.troussard@gmail.com>
2023-07-29 12:14:03 -0400

    Finishes interpolation refactoring
    - renames data.json to portfolio.json
    - adds a social.json file for social media resource configuration
    - adds a new layout css file
    - adds some color to the social media resources
    - updates ignore file to ignore the new json files
    - updates route file to pull in new files
    - passes title to home template
    - whitepsace cleanup
    - adds some hover affects to social media buttons

--- personal_website/routes.py
+++ personal_website/routes.py
@@ -12,6 +12,7 @@ import os, json
 from flask import render_template
 from personal_website import app
 
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -20,16 +21,28 @@ def home():
     :return: The home.html file is being returned.
     """
     # Read the path to the JSON file from the environment variable
-    data_path = os.environ.get('PORTFOLIO_DATA_PATH')
-    print(data_path)
+    portfolio_data_path = os.environ.get("PORTFOLIO_DATA_PATH")
+    social_data_path = os.environ.get("SOCIAL_DATA_PATH")
 
-    if data_path is None:
-        return "Error: Portfolio data path not set."
+    if portfolio_data_path is None:
+        app.logger.error("Portfolio data path not set.")
+    if social_data_path is None:
+        app.logger.error("Social data path not set.")
 
     # Load the JSON data from the file
-    with open(data_path, 'r', encoding='utf-8') as json_file:
-        portfolio_data = json.load(json_file)
-    return render_template("home.html", active_page="home", data=portfolio_data)
+    with open(portfolio_data_path, "r", encoding="utf-8") as profilio_file, open(
+        social_data_path, "r", encoding="utf-8"
+    ) as social_file:
+        portfolio_data = json.load(profilio_file)
+        social_data = json.load(social_file)
+    return render_template(
+        "home.html",
+        active_page="home",
+        portfolio=portfolio_data,
+        social=social_data,
+        title="Jacques Troussard",
+    )
+
 
 @app.route("/weight-tracker", methods=["GET"])
 def weight_tracker():
