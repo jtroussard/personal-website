590e5633b7a929b245093d442c7f18006730a1b2
jacques troussard <jacques.troussard@gmail.com>
2023-07-31 13:41:44 -0400

    testing unit tests with mocked data

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from personal_website import init_app
 
@@ -7,6 +8,15 @@ class TestRoutes(unittest.TestCase):
         self.app = init_app()
         self.app.testing = True
         self.client = self.app.test_client()
+
+        # Get the relative path to the 'mocks' directory inside the 'tests' directory.
+        tests_directory = os.path.dirname(os.path.abspath(__file__))
+        mocks_directory = os.path.join(tests_directory, 'mocks')
+
+        # Set the PORTFOLIO_DATA_PATH environment variable to the mocks directory path.
+        os.environ['PORTFOLIO_DATA_PATH'] = mocks_directory + "/mock_content_portfolio.json"
+        os.environ['SOCIAL_DATA_PATH'] = mocks_directory + "/mock_content_social.json"
+        os.environ['PROJECTS_DATA_PATH'] = mocks_directory + "/mock_content_projects.json"
 
     def test_home_route(self):
         response = self.client.get('/')
