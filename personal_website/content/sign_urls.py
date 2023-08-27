14d2bd7b8f3a5f26a570be78c8cae4da9e7af39e
jacques troussard <jacques.troussard@gmail.com>
2023-08-27 18:03:16 -0400

    [PROD] google docs were garbo, had to dig into the host server and fix the google storage client authentication process

--- personal_website/content/sign_urls.py
+++ personal_website/content/sign_urls.py
@@ -2,8 +2,8 @@
 Module: sign_urls
 Description: Provides utility functions for generating signed URLs for assets stored in Google Cloud Storage.
 """
-
 from google.cloud import storage
+from google.auth import compute_engine
 
 def signed_url_generator(
     environment, logger, bucket, bucket_path_to_file, kagis, force_refresh=False
@@ -20,18 +20,30 @@ def signed_url_generator(
     :return: The signed URL.
     """
     if environment == "development":
-        # Load the service account key explicitly for development environments.
-        # Pass the path to the service account key file downloaded from GCP.
+        # GCP Docs where dog shit on this process. Answer was in front of me the whole
+        # but was trying to follow some principles and the docs. Just store the key as a
+        # secret and pass it to the sotrage client. This ACP or auto authentication
+        # from the compute engine (mentioned in docs) is a giant pile of poo. And if
+        # there is a receipt to authenticate another way it isn't obvious and the docs 
+        # are a little bit all over the place so it is espically hard to line up
+        # the documentaion aritcles so that they all represent good and correct 
+        # information when taking into consideration your use case, 
+        # source resource type/env/service account/destination resource type... etc
         storage_client = storage.Client.from_service_account_json(kagis)
-        logger.info("storage client in developer mode")
-    else:
-        # Docs state compute engine in production env auto authenticates the
-        # service account for "free"
-        # https://cloud.google.com/docs/authentication/provide-credentials-adc#attached-sa
-        storage_client = storage.Client()
+        credentials_info = storage_client._credentials.to_authorized_user_info()
+        logger.info(f"gcp authentication in DEVELOPMENT mode")
 
-    # TODO: I wanted to be more elegant here but I will open an issue and revisit this
-    # conditional later, stuck in the on positions for now
+    elif environment == "production":
+        credentials = compute_engine.Credentials()
+        storage_client = storage.Client.from_service_account_json(kagis)
+        logger.info(f"gcp authentication in PRODUCTION mode")
+    else:
+        logger.error(f"invalid environment: {environment}")
+
+    # TODO: What to make this more elegant but for now just going to leave the tap on
+    # until I am more confident in the authentication pattern I am following
+    # (see above rant) then I will return to this and save myself a call based
+    # on the expiration diff of the signed url.
     if force_refresh:
         bucket_name = bucket
         blob = storage_client.bucket(bucket_name).blob(bucket_path_to_file)
